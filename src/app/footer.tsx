17d7b8034623a21d72fdafd37d849439e2082c68
Ankush Shah <ankushsahvlogs@gmail.com>
2024-11-23 10:14:56 +0530

    up

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,25 +4,25 @@ export function Footer() {
   return (
     <div className="hidden custom-md:flex h-40 bg-gray-100 mt-12 items-center"> {/* Use custom-md to hide below 901px */}
       <div className="container mx-auto flex justify-between items-center">
-        <Link className="text-blue-900 hover:text-blue-500" href="https://github.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://github.com/havenfutures">
           Github
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="https://youtube.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://youtube.com/havenfutures">
           Youtube
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="https://instagram.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://instagram.com/thehavenfutures">
           Instagram
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="/privacy">
-          Build with ❤️ By Ankush Shah
+        <Link className="text-blue-900 hover:text-blue-500" href="/">
+          Built with ❤️ By Haven Futures
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="https://x.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://x.com/havenfutures">
           Twitter
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="https://facebook.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://facebook.com/HavenFutures">
           Facebook
         </Link>
-        <Link className="text-blue-900 hover:text-blue-500" href="https://linkedin.com">
+        <Link className="text-blue-900 hover:text-blue-500" href="https://www.linkedin.com/company/havenfutures">
           LinkedIn
         </Link>
       </div>
